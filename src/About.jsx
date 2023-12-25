8fe8c9d84bca621398030179896b752123b9fc2e
Saurabh7651 <saurabhp.8382@gmail.com>
2023-12-25 21:01:01 +0530

    first commit

---
+++ src/About.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import web from "../src/images/hero-img.png";
+import { NavLink } from 'react-router-dom';
+
+const About = () => {
+  return (
+  <>
+    <section id="header" className='d-flex align items-centre'>
+      <div className='container-fluid'>
+        <div className='row'>
+            <div className='col-10 mx-auto'>
+             <div className='row'>
+              <div className='col-md-6 pt-5 pt-lg-0 order-2 order-lg-1 d-flex justify-content-center flex-column'>
+                <h1>
+                  Welcome to About page
+                   <strong className='brand-name'> Saurabh Pandey</strong>
+                </h1>
+                <h2 className='my-3'>
+                  We are the team of talented developer making websites
+                </h2>
+                <div className='mt-3'>
+                  <NavLink to='/contact' className='btn-get-started'>
+                    Contact Now
+                  </NavLink>
+                </div>
+              </div>
+
+              <div className='col-lg-6 order-1 order-lg-2 header-img'>
+                <img src= {web} className='img-fluid animated' alt='home img' />
+              </div>
+            </div>
+          </div>
+          </div>
+      </div>          
+      </section>
+  </>
+  );
+};
+
+export default About;
